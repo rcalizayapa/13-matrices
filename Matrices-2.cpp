5ad4e532e991f0982fd2536d18074cd5d49886e2
rcalizayapa <rcalizayapa@unjbg.edu.pe>
2024-07-15 10:49:58 -0500

    Se agrego la funcion para introducir los datos de orden de la matriz

---
+++ Matrices-2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n, s=0;
+	cout<<"Ingrese el orden de la matriz: ";cin>>n;
+
+	int matriz[n][n];
+	
+	for (int i=0;i<n;i++){
+		for (int j=0; j<n; j++){
+			cout<<"Ingrese el valor para la matriz[]"<<i<<"]["<<j<<"]:";cin>>matriz[i][j];
+		}
+	}
+    cout<<"Sumatoria de la diagonal secundaria de la matriz: "<<endl;
+    
+}
